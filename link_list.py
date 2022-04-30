767330fb03b83a02619a147c9923d0beae3bdd85
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-04-30 15:17:32 +0800

    Update link_list.py

--- link_list.py
+++ link_list.py
@@ -3,81 +3,90 @@
 """
 @File: link_list.py
 @Author: jiajing
-@Data: 2022/04/26
+@Data: 2022/04/40
 """
 
-from node import Node    
+import node
 
 class LinkList(object):
-    """ 单链表 """
+    """ 无序单链表 """
     def __init__(self):
-        self.head = Node   
+        self.head = None
 
     def isEmpty(self):
-        return self.head == None
+        """链表是否为空"""
+        reutrn self.head. == None
 
     def length(self):
-        current = self.head
+        """链表长度"""
+        cur = head
         count = 0
-        while current != None:
+        while cur != None:
             count += 1
-            count = count.getNext()
+            cur = cur.next
         return count
-        
-    def get(self, index):
-        """ 取链表中第index个节点的值。如果索引无效，则返回-1 """
-        if index < 0 or index >= self.length():
-            return 
+
+    def travel(self):
+        """遍历链表"""
         cur = self.head
-        for _ in range(index + 1):
-            cur = cur.next      #第一次循环后，cur为真正的头节点
-        return cur.value        #循环结束后，cur为index的节点
-    
-    def search(self, val):
-        current = self.head
-        found = False
-        while current != None and not found:
-            if current.getData() == val:
-                found = True
-            else:
-                current = current.getNext()
-        return found
-        
-    def addAtHead(self, val):
-        """ 在链表的第一个元素之前添加一个值为val的节点。插入后，新节点将成为链表的第一个节点 """
-        temp = Node(val)
-        temp.setNext(self.head)
-        self.head = temp
-        
-    def addAtTail(self, val):
-        """ 将值为 val 的节点追加到链表的最后一个元素 """
-        pass
-    
-    def addAtIndex(self, index, val):
-        """ 
-        在链表中的第index个节点前添加值为val的节点。
-        如果index等于链表的长度，则该节点将附加到链表的末尾。
-        如果index大于链表长度，不会插入节点。
-        如果index小于0，则在头部插入节点 
-        """
-        pass
-    
-    def remove(self, val):
-        current = self.head
-        previous = None
-        found = False
-        while not found:
-            if current.getData() == val:
-                found = True
-            else:
-                previous = current
-                current = current.getNext()
-        if previous == None:
-            self.head = current.getNext()
+        while cur != None:
+            print(cur.data)
+            cur = cur.next
+
+    def add(self, item):
+        """向链表中添加节点，头插法"""
+        node = node.Node(item)
+        node.next = self.head
+        self.head = node
+
+    def append(self, item):
+        """向链表中添加节点，尾插法"""
+        node = node.Node(item)
+        if self.is_empty():
+            self.head = None
         else:
-            previous.setNext(current.getNext())
-        
-    def deleteAtIndex(self, index):
-        """ 如果索引index有效，则删除链表中的第index个节点 """
-        pass
-       
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+    
+    def insert(self, pos, item):
+        """向指定位置添加节点"""
+        if pos < 0:
+            self.add(item)
+        elif pos > (self.length()-1):
+            self.append(item)
+        else:
+            pre = self.head
+            count = 0
+            while count < (pos -1):
+                count += 1
+                pre = pre.next
+            node = node.Node(item)
+            node.next = pre.next
+            pre.next = node
+
+    def remove(self, item):
+        """删除节点"""
+        cur = self.head
+        pre = None
+        while cur!= None:
+            if cur.data == item:
+                # 先判断此节点是否为头节点
+                if cur == self.head:
+                    self.head = cur.next
+                else:
+                    pre.next = cur.next
+            else:
+                pre = cur
+                cur = cur.next
+
+    def search(self, item):
+        """查找节点是否存在"""
+        cur = self.head
+        while cur != None:
+            if cur.data == item:
+                return True
+            else:
+                cur = cur.next
+        return False    
