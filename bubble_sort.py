a80365e09de8435014819daa5e9d661f7060b345
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-05-05 22:04:19 +0800

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# 冒泡排序
+class Solution(object):
+    def bubbleSort(self, arr):
+        if len(arr) <= 1:
+            return arr
+        for i in range(1, len(arr)):
+            for j in range(0, len(arr) - i):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+        return arr
+
+if __name__ == "__main__":
+    s = Solution()
+    arr = [1,4,9,3,7,5]
+    new_arr = s.bubbleSort(arr)
+    print(new_arr)
