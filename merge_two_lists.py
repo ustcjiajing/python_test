6d1507b3a2db2ab959b0695446daf717fb860f12
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-05-29 23:30:41 +0800

    Create merge_two_lists.py

---
+++ merge_two_lists.py
@@ -0,0 +1,38 @@
+"""
+21. 合并两个有序链表
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+"""
+
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists_1(self, list1, list2):
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+
+    def mergeTwoLists(self, list1, list2):
+        prehead = ListNode(-1)
+        prev = prehead
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        # 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = list1 if list1 is not None else list2
+        return prehead.next
