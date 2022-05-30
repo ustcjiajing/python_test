53997453d6c5df78b510fa524ffa0a28f751f631
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-05-30 09:38:51 +0800

    Update length_of_longest_substring.py

--- length_of_longest_substring.py
+++ length_of_longest_substring.py
@@ -6,3 +6,21 @@
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 """
 
+class Solution(object):        
+    def lengthOfLongestSubstring(self, s):
+        if not s:
+            return 0
+        max_len, hashset = 0, set()
+        left, right = 0, 0
+        n = len(s)
+
+        for i in range(n):
+            right += 1
+            while s[i] in hashset:
+                hashset.remove(s[left])
+                left += 1
+                right -= 1
+            if right > max_len:
+                max_len = right
+            hashset.add(s[i])
+        return max_len
