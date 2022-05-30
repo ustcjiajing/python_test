97005cda24c91470a6d7ce20253868d4f25798cf
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-05-30 23:07:38 +0800

    Update search.py

--- search.py
+++ search.py
@@ -3,4 +3,14 @@
 
 class Solution(object):
   def search(self, nums, target):
-    
+      left = 0
+      right = len(nums) - 1
+      while left <= right:
+          mid = (left + right) / 2
+          if nums[mid] == target:
+              return mid
+          if nums[mid] > target:
+              right = mid - 1
+          else:
+              left = mid + 1
+      return -1
