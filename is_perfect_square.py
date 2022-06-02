58588f1c1ec9dd745987c2de39ad04deab178b8f
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-06-02 11:39:39 +0800

    Update is_perfect_square.py

--- is_perfect_square.py
+++ is_perfect_square.py
@@ -2,8 +2,27 @@
 367. 有效的完全平方数
 给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
 进阶：不要 使用任何内置的库函数，如  sqrt 。
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/valid-perfect-square
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+输入：num = 16
+输出：true
+输入：num = 14
+输出：false
 """
+
+class Solution(object):
+    #时间复杂度：O(logn)
+    #空间复杂度：O(1)
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        left, right = 0, num
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            if mid * mid > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
