23e73310a4b348e81fe66360d95d47b98b089be0
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-06-04 17:16:57 +0800

    Create climb_stairs.py

---
+++ climb_stairs.py
@@ -0,0 +1,18 @@
+"""
+70. 爬楼梯
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+输入：n = 2
+输出：2
+解释：有两种方法可以爬到楼顶。
+1. 1 阶 + 1 阶
+2. 2 阶
+"""
+
+class Solution(object):
+    def climbStairs(self, n):
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i -1] + dp[i-2]
+        return dp[n]
