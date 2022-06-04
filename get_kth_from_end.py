d288587045069f9403876cfbae677e353f0a0e45
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-06-04 23:06:44 +0800

    Create get_kth_from_end.py

---
+++ get_kth_from_end.py
@@ -0,0 +1,30 @@
+"""
+剑指 Offer 22. 链表中倒数第k个节点
+输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+给定一个链表: 1->2->3->4->5, 和 k = 2.
+返回链表 4->5.
+"""
+
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getKthFromEnd(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        quick = slow = head
+        while k > 0:
+            quick = quick.next
+            k -= 1
+        while quick is not None:
+            quick = quick.next
+            slow = slow.next
+        return slow
