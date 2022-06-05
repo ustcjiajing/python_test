dd4346a05cf6e77a0b5e15516e09a3798412a7d6
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-06-05 15:53:36 +0800

    Create min_depth.py

---
+++ min_depth.py
@@ -0,0 +1,30 @@
+"""
+111. 二叉树的最小深度
+给定一个二叉树，找出其最小深度。
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+说明：叶子节点是指没有子节点的节点。
+输入：root = [3,9,20,null,null,15,7]
+输出：2
+"""
+
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root):
+        if not root:
+            return 0
+
+        que = collections.deque([(root, 1)])
+        while que:
+            node, depth = que.popleft()
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                que.append((node.left, depth + 1))
+            if node.right:
+                que.append((node.right, depth + 1))
+        
+        return 0
