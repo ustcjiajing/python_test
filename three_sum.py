682a0adc3b138c42a125d88f25fade59fca9a32c
ustcjiajing <67005440+ustcjiajing@users.noreply.github.com>
2022-07-13 18:05:16 +0800

    Create three_sum.py

---
+++ three_sum.py
@@ -0,0 +1,28 @@
+# 15. 三数之和
+# https://leetcode.cn/problems/3sum/
+
+class Solution(object):
+    def threeSum(self, nums):
+        # 排序 + 双指针，（O(n2), O(1)）
+        nums.sort()
+        res, k = [], 0
+
+        for k in range(len(nums) - 2):
+            if nums[k] > 0: break # 1. because of j > i > k.
+            if k > 0 and nums[k] == nums[k - 1]: continue # 2. skip the same `nums[k]`.
+            i, j = k + 1, len(nums) - 1
+            while i < j: # 3. double pointer
+                s = nums[k] + nums[i] + nums[j]
+                if s < 0:
+                    i += 1
+                    while i < j and nums[i] == nums[i - 1]: i += 1
+                elif s > 0:
+                    j -= 1
+                    while i < j and nums[j] == nums[j + 1]: j -= 1
+                else:
+                    res.append([nums[k], nums[i], nums[j]])
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]: i += 1
+                    while i < j and nums[j] == nums[j + 1]: j -= 1
+        return res
